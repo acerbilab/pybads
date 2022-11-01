2f3febf9eea3d432b17c240d053547c8ec0a1381
GurjeetSinghSangra <gurjeet.singh.sangra@gmail.com>
2022-11-01 18:29:37 +0100

    fix: removed wrong comment

--- pybads/stats/get_hpd.py
+++ pybads/stats/get_hpd.py
@@ -30,7 +30,6 @@ def get_hpd(X: np.ndarray, y: np.ndarray, hpd_frac: float = 0.8):
     N, D = X.shape
 
     # Subsample high posterior density dataset.
-    # Sort by descending order, not ascending.
     order = np.argsort(y, axis=None)[::]
     hpd_N = round(hpd_frac * N)
     indices = order[0:hpd_N]
