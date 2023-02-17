e524ba426e702d0c6812cd4d2ff034ba0470681e
GurjeetSinghSangra <gurjeet.singh.sangra@gmail.com>
2023-02-17 16:59:26 +0100

    fix: move to pyproject.toml dependency settings as in PyVBMC

--- setup.py
+++ setup.py
@@ -1,29 +1,9 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
 
-setup(
-    name="pybads",
-    version="0.0.1",
-    description="Bayesian Adaptive Direct Search",
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "pytest",
-        "sphinx",
-        "numpydoc",
-        "cma",
-        "corner",
-        "imageio",
-        "kaleido",
-        "myst_nb",
-        "numpydoc",
-        "sphinx",
-        "sphinx-book-theme",
-        "pylint",
-        "pytest",
-        "pytest-cov",
-        "pytest-mock",
-        "pytest-rerunfailures",
-    ],
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        use_scm_version=True,
+        setup_requires=["setuptools_scm"],
+    )
