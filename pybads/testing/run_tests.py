6b3f4b69a11bf06c6afbe345aa5cccc45e6f93b7
Gurjeet Singh Sangra <gurjeet.singh.sangra@gmail.com>
2023-02-18 21:26:17 +0100

    fix: added manifest file for pybads release, and refactored(moved) testing files into pybads directory (#27)

---
+++ pybads/testing/run_tests.py
@@ -0,0 +1,6 @@
+from testing.bads import test_init_conf as b_conf_t
+from testing.decorators import test_handle_0D_1D_input_decorator as dt
+from testing.variable_transformer import test_variable_transformer as vtt
+
+dt.test_0D_kwarg()
+vtt.test_init_type_3()
--- tests/run_tests.py
+++
@@ -1,6 +0,0 @@
-from tests.bads import test_init_conf as b_conf_t
-from tests.decorators import test_handle_0D_1D_input_decorator as dt
-from tests.variable_transformer import test_variable_transformer as vtt
-
-dt.test_0D_kwarg()
-vtt.test_init_type_3()
